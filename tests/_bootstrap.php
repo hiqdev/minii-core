655c81bbccd6e6edb4867f744804b2b1d43b1d6c
Andrii Vasyliev <sol@hiqdev.com>
2015-12-29 12:01:05 +0000

    trying to enable tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -8,7 +8,7 @@ define('YII_DEBUG', true);
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
-require_once(__DIR__ . '/../vendor/autoload.php');
+$composerAutoload = __DIR__ . '/../vendor/autoload.php';
 if (is_file($composerAutoload)) {
     require_once($composerAutoload);
 } else {
